12db742c4af40e4c42cbd28e6fa6b5e386b1538c
Jamie Phillips <jmpjamie@gmail.com>
2022-05-01 21:11:51 +0100

    added footer

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const menuItem = document.querySelectorAll(".menu-item");
 const body = document.body;
 
 menuBtn.addEventListener("click", () => {
+  console.log("no scroll");
   body.classList.toggle("no-scroll");
   menuBtn.classList.toggle("active");
   menuItems.classList.toggle("open");
